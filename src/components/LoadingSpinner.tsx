13cdc305425a581ca0d8449b479a407fa340e685
1Cryptify <austindouglas562@gmail.com>
2026-02-10 11:08:53 +0100

    ok

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Loader2 } from 'lucide-react';
-import styles from './LoadingSpinner.module.css';
+import { FiLoader } from 'react-icons/fi';
 
 const LoadingSpinner: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <Loader2 className={styles.spinner} />
-    </div>
+
   );
 };
 
