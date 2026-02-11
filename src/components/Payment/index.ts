19582ca2db8cad0978e9ef0945d7ac9f22297de4
1Cryptify <austindouglas562@gmail.com>
2026-02-11 01:22:15 +0100

    ok

--- src/components/Payment/index.ts
+++
@@ -1,4 +0,0 @@
-export { OfferGroupPayment } from './OfferGroupPayment';
-export { OfferPaymentForm } from './OfferPaymentForm';
-export { ProductPaymentForm } from './ProductPaymentForm';
-export { PaymentVerification } from './PaymentVerification';
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 // Export des pages de paiement
-export { PaymentPage } from './PaymentPage';
+export { PaymentPage } from './PayPage';
 export { PaymentCheckoutPage } from './PaymentCheckoutPage';
 export { PaymentSuccessPage } from './PaymentSuccessPage';
 export { PaymentFailedPage } from './PaymentFailedPage';
